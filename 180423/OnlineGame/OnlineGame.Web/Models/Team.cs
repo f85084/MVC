3269c0aba61734413722c2ee81096e57330eb8ef
Anna Chen <anna22_chen@ehsn.com.tw>
2018-04-25 23:19:45 +0800

    Revert "Revert "Initial commit""
    This reverts commit f93fd7d765aef6ba4eec996d14aee10547edc054.

---
+++ 180423/OnlineGame/OnlineGame.Web/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace OnlineGame.Web.Models
+{
+    //[Table("Gamer"]
+    [Table("Team", Schema = "dbo")]
+    public class Team
+    {
+        public int Id { get; set; }
+        [Column("Name")]
+        public string Name { get; set; }
+        public List<Gamer> Gamers { get; set; }
+    }
+}
