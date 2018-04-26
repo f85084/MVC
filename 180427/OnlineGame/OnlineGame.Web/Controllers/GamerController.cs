d832f48ee46993f3c45e24df2e8d341abd86b979
Anna <f85084@gmail.com>
2018-04-27 00:33:50 +0800

    0427_home_2

---
+++ 180427/OnlineGame/OnlineGame.Web/Controllers/GamerController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineGame.Web.Models;
+
+namespace OnlineGame.Web.Controllers
+{
+    public class GamerController : Controller
+    {
+        private OnlineGameContext db = new OnlineGameContext();
+
+        // GET: Gamers
+        public async Task<ActionResult> Index()
+        {
+            var gamer = db.Gamer.Include(g => g.Team);
+            return View(await gamer.ToListAsync());
+        }
+
+        // GET: Gamers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gamer gamer = await db.Gamer.FindAsync(id);
+            if (gamer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gamer);
+        }
+
+        // GET: Gamers/Create
+        public ActionResult Create()
+        {
+            ViewBag.TeamId = new SelectList(db.Team, "Id", "Name");
+            return View();
+        }
+
+        // POST: Gamers/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Gender,City,DateOfBirth,EmailAddress,Score,ProfileUrl,GameMoney,TeamId,RolePhoto,RolePhotoAltText")] Gamer gamer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gamer.Add(gamer);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TeamId = new SelectList(db.Team, "Id", "Name", gamer.TeamId);
+            return View(gamer);
+        }
+
+        // GET: Gamers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gamer gamer = await db.Gamer.FindAsync(id);
+            if (gamer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TeamId = new SelectList(db.Team, "Id", "Name", gamer.TeamId);
+            return View(gamer);
+        }
+
+        // POST: Gamers/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Gender,City,DateOfBirth,EmailAddress,Score,ProfileUrl,GameMoney,TeamId,RolePhoto,RolePhotoAltText")] Gamer gamer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gamer).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TeamId = new SelectList(db.Team, "Id", "Name", gamer.TeamId);
+            return View(gamer);
+        }
+
+        // GET: Gamers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gamer gamer = await db.Gamer.FindAsync(id);
+            if (gamer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gamer);
+        }
+
+        // POST: Gamers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Gamer gamer = await db.Gamer.FindAsync(id);
+            db.Gamer.Remove(gamer);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
