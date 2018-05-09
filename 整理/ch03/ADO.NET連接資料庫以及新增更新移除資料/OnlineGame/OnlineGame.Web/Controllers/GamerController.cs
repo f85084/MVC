52b4c6b8f558298ffe268b0874a7f063dba71042
Anna Chen <anna22_chen@ehsn.com.tw>
2018-05-09 18:08:51 +0800

    整理報告

---
+++ 整理/ch03/ADO.NET連接資料庫以及新增更新移除資料/OnlineGame/OnlineGame.Web/Controllers/GamerController.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BusinessLayer;
+using OnlineGame.Web.Data;
+using Gamer = OnlineGame.Web.Models.Gamer;
+
+namespace OnlineGame.Web.Controllers
+{
+    public class GamerController : Controller
+    {
+        // http://localhost/OnlineGame.Web/Gamer/Details
+        //public ActionResult Details()
+        //{
+        //    var gamer = new Gamer
+        //    {
+        //        Id = 1,
+        //        Name = "Name1",
+        //        Gender = "Male",
+        //        City = "City1"
+        //    };
+        //    return View(gamer);
+        //}
+
+
+        // http://localhost/OnlineGame.Web/Gamer/Details
+        // http://localhost/OnlineGame.Web/Gamer/Details/1
+        // http://localhost/OnlineGame.Web/Gamer/Details/2
+        // http://localhost/OnlineGame.Web/Gamer/Details/3
+        // http://localhost/OnlineGame.Web/Gamer/Details/4
+        public ActionResult Details(int id = 0)
+        {
+            var onlineGameContext = new OnlineGameContext();
+            Gamer gamer;
+            if (id == 0)
+            {
+                gamer = new Gamer
+                {
+                    Id = 0,
+                    Name = "Name0",
+                    Gender = "NULL",
+                    City = "NULL"
+                };
+                // or you may throw exception here.
+            }
+            else
+            {
+                gamer = onlineGameContext.Gamers.Single(p => p.Id == id);
+                //Throws exception if can not find the single entity
+            }
+            return View(gamer);
+        }
+
+
+
+        public ActionResult Index(int teamId)
+        {
+            //Entity Framework
+            OnlineGameContext context = new OnlineGameContext();
+            List<Gamer> gamers = context.Gamers.Where(gamer => gamer.TeamId == teamId).ToList();
+            return View(gamers);
+        }
+
+
+        public ActionResult Index2()
+        {
+            //Ado.Net
+            GamerBusinessLayer gamerBusinessLayer = new GamerBusinessLayer();
+            List<BusinessLayer.Gamer> gamers = gamerBusinessLayer.Gamers.ToList();
+            return View(gamers);
+        }
+
+
+        //[HttpGet] attribute means it only respond to the "GET" request. 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // 1. Retrieve form data using FormCollection
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            ////FormCollection implement C# indexer. 
+            ////See each key and value of formCollection.
+            //foreach (string key in formCollection.AllKeys)
+            //{
+            //    Response.Write($"key=={key}, {formCollection[key]}, <br/>");
+            //}
+
+            int teamId;
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer
+            {
+                Name = formCollection["Name"],
+                Gender = formCollection["Gender"],
+                City = formCollection["City"],
+                DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]),
+                TeamId = int.TryParse(formCollection["TeamId"], out teamId) ? teamId : 0
+            };
+
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+
+        // 2. Retrieve form data using name attribute of input tag from cshtml
+        [HttpPost]
+        public ActionResult Create2(string name, string gender, string city, DateTime dateOfBirth, int teamId)
+        {
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer
+            {
+                Name = name,
+                Gender = gender,
+                City = city,
+                DateOfBirth = dateOfBirth,
+                TeamId = teamId
+            };
+
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+
+
+        // 3. Retrieve form data using model binding
+        [HttpPost]
+        public ActionResult Create3(BusinessLayer.Gamer gamer)
+        {
+            //if any of input is not valid.
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+                //Go to Create.cshtml, 
+                //so users can correct their input value.
+            }
+
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+
+        // 4. Retrieve form data using model binding by UpdateModel() or TryUpdateModel()
+        [HttpPost]
+        [ActionName("Create4")]
+        public ActionResult Create_Post()
+        {
+            //if any of input is not valid.
+            if (!ModelState.IsValid)
+            {
+                return View("Create");
+                //Go to Create.cshtml, 
+                //so users can correct their input value.
+            }         
+            GamerBusinessLayer gamerBusinessLayer =
+                new GamerBusinessLayer();
+
+            BusinessLayer.Gamer gamer = new BusinessLayer.Gamer();
+            //UpdateModel<BusinessLayer.Gamer>(gamer);
+            //UpdateModel(gamer);
+            TryUpdateModel(gamer);
+            //1.
+            // UpdateModel() and TryUpdateModel() inspects all the HttpRequest inputs 
+            // such as posted Form data, QueryString, 
+            // Cookies and Server variables and populate the gamer object.
+
+            gamerBusinessLayer.AddGamer(gamer);
+            return RedirectToAction("Index2");
+        }
+
+    }
+}
+
+/*
+1.
+//var onlineGameContext = new OnlineGameContext();
+//Gamer gamer = onlineGameContext.Gamers.Single(p => p.Id == id);
+When user request, EntityFramework will request the data from the database
+and sotre its data into a temp place called DBSet.
+onlineGameContext.Gamers is a DBSet which is kind of temp place to store the Gamer Table Data.
+We use LINQ to map the Gamer Table Column id to Gamer Model property, id.
+Thus, we can get the gamer entity from Gamer Table by its id.
+Then store gamer entity data into Gamer Model object.
+Thus, each Gamer Model object is a temp place to store each Gamer Table entity from the database.
+Then we pass the Gamer Model object as the ViewModel,
+Thus, the Details.cshtml view can use the values from Gamer Model object
+which is actually the temp place to store Gamer Table entity data.
+
+-------------------------------
+2.
+//[HttpGet]
+//public ActionResult Create()
+The GET request will direct to Views/Gamer/Create.cshtml.
+
+-------------------------------
+3.
+//[HttpPost]
+-----------------
+3.1.
+//[HttpPost]
+//public ActionResult Create(FormCollection formCollection)
+Retrieve form data using FormCollection.
+The key is the name attribute of input or select tag from cshtml.
+-----------------
+3.2.
+//[HttpPost]
+//public ActionResult Create2(string name, string gender, string city, DateTime dateOfBirth, int teamId)
+Retrieve form data using name attribute of input tag from cshtml.
+3.2.1.
+string name is from
+//<input class="form-control text-box single-line" id="Name" name="Name" type="text" value="">
+3.2.2.
+string gender is from
+//<select id="Gender" name="Gender">...</select>
+3.2.3.
+string city is from
+<input class="form-control text-box single-line" id="City" name="City" type="text" value="">
+3.2.4.
+DateTime dateOfBirth is from
+//<input class="form-control text-box single-line" data-val="true" data-val-date="The field DateOfBirth must be a date." data-val-required="The DateOfBirth field is required." id="DateOfBirth" name="DateOfBirth" type="datetime" value="">
+3.2.5.
+int teamId is from
+//<input class="form-control text-box single-line" data-val="true" data-val-number="The field TeamId must be a number." data-val-required="The TeamId field is required." id="TeamId" name="TeamId" type="number" value="">
+-----------------
+3.3.
+//[HttpPost]
+//public ActionResult Create3(BusinessLayer.Gamer gamer)
+If the view has a lot of input, 
+then the previous two ways is not a good idea.
+It is always better to retrieve form data using model binding.
+The model of the cshtml is BusinessLayer.Gamer, 
+so we can pass the model object into HttpPost action.
+The property value of model object will contain the value 
+from input or select tag from cshtml based on name attribute.
+-----------------
+3.4.
+//[HttpPost]
+//[ActionName("Create4")]
+//public ActionResult Create_Post()
+Retrieve form data using model binding by UpdateModel() or TryUpdateModel()
+...
+//BusinessLayer.Gamer gamer = new BusinessLayer.Gamer();
+////UpdateModel<BusinessLayer.Gamer>(gamer);
+////UpdateModel(gamer);
+//TryUpdateModel(gamer);
+3.4.1.
+UpdateModel() and TryUpdateModel() inspects all the HttpRequest inputs 
+such as posted Form data, QueryString, 
+Cookies and Server variables and populate the gamer object.
+3.4.2.
+UpdateModel() throws an exception if validation fails.
+TryUpdateModel() will never throw an exception and 
+return false if validation fails.
+*/
---
+++ 整理/ch03/ADO.NET連接資料庫以及新增更新移除資料/OnlineGame/T004_MvcConventions_AdoDotNet_BusinessLayer.sql
@@ -0,0 +1,120 @@
+--1. Drop if it exists
+--Drop Table if it exists.
+IF ( EXISTS ( SELECT    *
+              FROM      INFORMATION_SCHEMA.TABLES
+              WHERE     TABLE_NAME = 'Gamer' ) )
+    BEGIN
+        TRUNCATE TABLE Gamer;
+        DROP TABLE Gamer;
+    END;
+GO -- Run the previous command and begins new batch
+IF ( EXISTS ( SELECT    *
+              FROM      INFORMATION_SCHEMA.TABLES
+              WHERE     TABLE_NAME = 'Team' ) )
+    BEGIN
+        TRUNCATE TABLE Team;
+        DROP TABLE Team;
+    END;
+GO -- Run the previous command and begins new batch
+
+--Drop Stored Procedure if it exists.
+--IF OBJECT_ID('spSearchGamer') IS NOT NULL
+IF ( EXISTS ( SELECT    *
+              FROM      INFORMATION_SCHEMA.ROUTINES
+              WHERE     ROUTINE_TYPE = 'PROCEDURE'
+                        AND LEFT(ROUTINE_NAME, 3) NOT IN ( 'sp_', 'xp_', 'ms_' )
+                        AND SPECIFIC_NAME = 'spGetGamers' ) )
+    BEGIN
+        DROP PROCEDURE spGetGamers;
+    END;
+GO -- Run the previous command and begins new batch
+--IF OBJECT_ID('spSearchGamer') IS NOT NULL
+IF ( EXISTS ( SELECT    *
+              FROM      INFORMATION_SCHEMA.ROUTINES
+              WHERE     ROUTINE_TYPE = 'PROCEDURE'
+                        AND LEFT(ROUTINE_NAME, 3) NOT IN ( 'sp_', 'xp_', 'ms_' )
+                        AND SPECIFIC_NAME = 'spAddGamer' ) )
+    BEGIN
+        DROP PROCEDURE spAddGamer;
+    END;
+GO -- Run the previous command and begins new batch
+
+
+--2. Create Table
+CREATE TABLE Team
+    (
+      Id INT PRIMARY KEY
+             IDENTITY(1, 1)
+             NOT NULL ,
+      [Name] NVARCHAR(100) NULL
+    );
+GO -- Run the previous command and begins new batch
+CREATE TABLE Gamer
+    (
+      Id INT PRIMARY KEY
+             IDENTITY(1, 1)
+             NOT NULL ,
+      [Name] NVARCHAR(100) NULL ,
+      Gender NVARCHAR(10) NULL ,
+      City NVARCHAR(50) NULL ,
+      DateOfBirth DATETIME NULL ,
+      TeamId INT FOREIGN KEY REFERENCES Team ( Id )
+    );
+GO -- Run the previous command and begins new batch
+
+
+--3. Insert Data
+INSERT  Team
+VALUES  ( N'Team1' );
+INSERT  Team
+VALUES  ( N'Team2' );
+INSERT  Team
+VALUES  ( N'Team3' );
+
+INSERT  Gamer
+VALUES  ( N'Name01 ABB', N'Male', N'City01', '1979/4/28', 1 );
+INSERT  Gamer
+VALUES  ( N'Name02 CDDE', N'Female', N'City03', '1981/7/24', 2 );
+INSERT  Gamer
+VALUES  ( N'Name03 FIJK', N'Female', N'City01', '1984/12/5', 3 );
+INSERT  Gamer
+VALUES  ( N'Name04 LMOPPQ', N'Male', N'City02', '1983/5/29', 1 );
+INSERT  Gamer
+VALUES  ( N'Name05 QRSTT', N'Male', N'City01', '1979/6/20', 3 );
+INSERT  Gamer
+VALUES  ( N'Name06 TUVVX', N'Female', N'City03', '1984/5/15', 3 );
+INSERT  Gamer
+VALUES  ( N'Name07 XYZZXX', N'Female', N'City01', '1986/4/29', 2 );
+INSERT  Gamer
+VALUES  ( N'Name08 ABBCDE', N'Male', N'City02', '1985/7/28', 1 );
+INSERT  Gamer
+VALUES  ( N'Name09 QRSTTUVXX', N'Male', N'City02', '1983/4/16', 1 );
+GO -- Run the previous command and begins new batch
+
+
+--4. SP
+CREATE PROCEDURE spGetGamers
+AS
+    BEGIN
+        SELECT  *
+        FROM    Gamer;
+    END;
+GO -- Run the previous command and begins new batch
+
+CREATE PROCEDURE spAddGamer
+    (
+      @Name NVARCHAR(50) ,
+      @Gender NVARCHAR(10) ,
+      @City NVARCHAR(50) ,
+      @DateOfBirth DateTime ,
+      @TeamId INT
+    )
+AS
+    BEGIN  
+        INSERT  INTO Gamer
+        VALUES  ( @Name, @Gender, @City, @DateOfBirth, @TeamId );  
+    END;
+GO -- Run the previous command and begins new batch
+
+--EXEC spGetGamers
+--GO -- Run the previous command and begins new batch
